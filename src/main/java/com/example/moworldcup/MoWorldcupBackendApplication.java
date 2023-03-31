9feaa08bf709556ae76c7e0a5b0b5270170294ef
zsr12383 <zsr123@naver.com>
2023-03-31 23:32:38 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/example/moworldcup/MoWorldcupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moworldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoWorldcupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoWorldcupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moworldcup/MoWorldcupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moworldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoWorldcupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
