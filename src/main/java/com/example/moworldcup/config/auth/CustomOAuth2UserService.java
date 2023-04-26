b5c95989c694532cab633911e7ad4d39ee754229
zsr12383 <zsr123@naver.com>
2023-04-26 19:41:22 +0900

    fix: local 모드 cors 허용 설정

--- src/main/java/com/example/moworldcup/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/example/moworldcup/config/auth/CustomOAuth2UserService.java
@@ -28,7 +28,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
@@ -45,7 +45,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 attributes.getAttributes(),
                 attributes.getNameAttributeKey());
     }
-    
+
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepository.findByEmail(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName()))
--- src/main/java/com/example/moworldcup/config/auth/SecurityConfig.java
+++ src/main/java/com/example/moworldcup/config/auth/SecurityConfig.java
@@ -2,17 +2,25 @@ package com.example.moworldcup.config.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -25,26 +33,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors().and()
-            .csrf().disable()
-            .headers().frameOptions().disable()
-            .and()
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/api/v1/topic/**", "/profile").permitAll()
-                .anyRequest().authenticated()
-            )
-            // .requestMatchers("/api/v1/**")
-            // .hasRole(Role.USER.name())
-//            .formLogin((form) -> form
-//                .loginPage("/login")
-//                .permitAll()
-//            )
-            .logout()
-            .logoutSuccessUrl("/")
-            .and()
-            .oauth2Login()
-            .userInfoEndpoint()
-            .userService(customOAuth2UserService);
+                .httpBasic().disable()
+                .cors(withDefaults())
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/api/v1/topic/**", "/profile").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()//.loginPage("/login")
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000/"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
